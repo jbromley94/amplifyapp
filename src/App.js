7d41fd011df98cbd3cc48c2d8474f29cd0b98821
Jordan Bromley <jordan.bromley@cdl.co.uk>
2020-04-16 07:56:24 +0100

    Rejoice for the CD AWS AMplify tool

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload, and celebrate when CD
           works when I push to Master! :O
         </p>
+        <p>
+          Though it takes about 2 to 3 min to fully build, this is a pretty darn
+          useful tool if we hoook it up to master branch and follow standard
+          coding practices!
+        </p>
+        <a href="https://youtu.be/MiXgOQ9_-RI?t=111" target="blank">
+          REJOICE!!!!!!!!!!
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
